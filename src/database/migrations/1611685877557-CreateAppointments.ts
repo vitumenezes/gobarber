ccb41f973e0726e57747fe39019dcdece6921ba3
Victor Menezes <victormenezez15@gmail.com>
2021-01-26 16:30:53 -0300

    TypeORM repository

--- src/database/migrations/1611685877557-CreateAppointments.ts
+++ src/database/migrations/1611685877557-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1611685877557
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
