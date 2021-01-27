76f35ba73a3ba7153e2b328a37c013fe87209b10
Victor Menezes <victormenezez15@gmail.com>
2021-01-26 21:42:37 -0300

    Model and user migration

--- src/database/migrations/1611706480241-CreateUsers.ts
+++ src/database/migrations/1611706480241-CreateUsers.ts
@@ -1,11 +1,10 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export default class CreateAppointments1611685877557
-  implements MigrationInterface {
+export default class CreateUsers1611706480241 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
-        name: 'appointments',
+        name: 'users',
         columns: [
           {
             name: 'id',
@@ -15,13 +14,18 @@ export default class CreateAppointments1611685877557
             default: 'uuid_generate_v4()',
           },
           {
-            name: 'provider',
+            name: 'name',
             type: 'varchar',
             isNullable: false,
           },
           {
-            name: 'date',
-            type: 'timestamp with time zone',
+            name: 'email',
+            type: 'varchar',
+            isUnique: true,
+          },
+          {
+            name: 'password',
+            type: 'varchar',
             isNullable: false,
           },
           {
@@ -40,6 +44,6 @@ export default class CreateAppointments1611685877557
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropTable('appointments');
+    await queryRunner.dropTable('users');
   }
 }
